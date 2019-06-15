71a779f093b3b91060d07a2f5e64b801e178a0cb
mlevental <m.levental@web.de>
2019-06-15 18:32:52 +0200

    setup project

---
+++ kinokasse/src/main/java/com/levental/kinokasse/KinokasseApplication.java
@@ -0,0 +1,13 @@
+package com.levental.kinokasse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KinokasseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KinokasseApplication.class, args);
+	}
+
+}
