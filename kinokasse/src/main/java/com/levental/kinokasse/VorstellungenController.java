9078232fe34ac9f74c5737867dbc0e89cfa9ed48
mlevental <m.levental@web.de>
2019-06-24 22:39:59 +0200

    displaying an overview of vorstellungen on the start page

--- kinokasse/src/main/java/com/levental/kinokasse/VorstellungenController.java
+++ kinokasse/src/main/java/com/levental/kinokasse/VorstellungenController.java
@@ -1,13 +1,17 @@
 package com.levental.kinokasse;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.levental.kinokasse.dao.VorstellungDao;
 
 @Controller
 public class VorstellungenController {
 
 	@RequestMapping("/")
-	public String vorstellungen() {
+	public String vorstellungen(Model model) {
+		model.addAttribute("vorstellungen", new VorstellungDao().getAll());
 		return "vorstellungen";
 	}
 }
